5a27fc3e1305aba9c097f993e108fc65eae70b55
Zelmax4 <maxence.godin@reseau.eseo.fr>
2024-05-24 09:38:05 +0200

    re-organization : finalized

---
+++ BEA_TeamCoocker/Assets/Script/Scriptable_Objects/Source_Scripts/Player.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "P",menuName = "SO/Player", order = 0)]
+public class Player : ScriptableObject
+{
+    
+}
